9a36f1ae243cf106bc52a64edf907d109cbf1359
dean <deanraguso@gmail.com>
2021-03-07 20:11:00 +1100

    Create the custom URL

--- script.js
+++ script.js
@@ -4,6 +4,14 @@
 const prompt = require('prompt-sync')();
 
 //Get user input
-const city = prompt("Please enter the name of a city: ");
-const state = prompt("Please enter the name of the state of that city: ");
+const city = prompt("Please enter the name of a city: ").toLocaleLowerCase();
+const state = prompt("Please enter the name of the state of that city: ").toLocaleLowerCase();
 
+//Construct URL
+const prelim = "&appid="
+const APIkey = "9f49ec1e09fbf705c91e6c289950c78b";
+let user_URL = "api.openweathermap.org/data/2.5/weather?q=";
+
+user_URL += city + "," + state + prelim + APIkey;
+
+console.log(user_URL);
